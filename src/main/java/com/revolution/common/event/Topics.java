655c9cb7c6e0d825c18ca9e31bb6f46d84a26f2d
karolwolny <kwolny31@gmail.com>
2025-03-27 15:36:24 +0100

    Add topics

---
+++ src/main/java/com/revolution/common/event/Topics.java
@@ -0,0 +1,10 @@
+package com.revolution.auth.service.api;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Topics {
+
+    public static final String REGISTER_TOPIC = "register-user-topic";
+}
