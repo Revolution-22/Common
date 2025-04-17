e5c0609d49d48a9a8b1b4675cdd4203b2a14a971
karolwolny <kwolny31@gmail.com>
2025-04-17 15:01:32 +0200

    Update UserResponse

--- src/main/java/com/revolution/common/response/UserResponse.java
+++ src/main/java/com/revolution/common/response/UserResponse.java
@@ -3,6 +3,7 @@ package com.revolution.common.response;
 import java.util.Set;
 
 public record UserResponse(
+        long userId,
         String email,
         Set<String> roles,
         String token,
